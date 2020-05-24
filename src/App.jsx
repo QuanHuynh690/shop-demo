9ce7382a6dd66c0bd9d591f80903280d31b86662
QuanHuynh690 <quanhuynhminh2112@gmail.com>
2020-05-24 18:21:03 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Layout from "./components/Layout";
+import Content from "./components/Content";
+import ProductItem from "./components/ProductItem";
+import SideBar from "./components/SideBar";
 
+const products = [
+  {
+    type: "Furniture",
+    name: "Iphone 999",
+    imageURL:
+      "https://media3.scdn.vn/img4/2020/03_16/FBeRVqPng49HKEg1qnqs_simg_b5529c_250x250_maxb.jpg",
+    price: "120.000 VND",
+    discountPrice: "100.000 VND",
+  },
+  {
+    type: "Tablet",
+    name: "Samsung Pro 123456",
+    imageURL:
+      "https://media3.scdn.vn/img4/2020/03_04/O6MAvAed87qmmdqsFaYw_simg_b5529c_250x250_maxb.jpg",
+    price: "720.000 VND",
+    discountPrice: "520.000 VND",
+  },
+  {
+    type: "Baby",
+    name: "Bobby",
+    imageURL:
+      "https://media3.scdn.vn/img4/2020/03_18/fx0jRvqJ7wVYmcj9swIb_simg_b5529c_250x250_maxb.jpg",
+    price: "110.000 VND",
+    discountPrice: "78.000 VND",
+  },
+];
 
-function SaleButton(props){
-    return (
-        <p type="text"> Sale</p>
-    )
+function App() {
+  return (
+    <Layout>
+      <Content>
+        <ProductItem 
+        
+            imageURL={products[0].imageURL}
+            type={products[0].type}
+            name={products[0].name}
+            price={products[0].price}
+            discountPrice={products[0].discountPrice}
+            
+            
+        />
+      </Content>
+      <SideBar />
+    </Layout>
+  );
 }
-function Image(props){
-    return(
-        <img src={props.imageURL}/>
-    )
-}   
-function Price(props){
-    return (
-        <div>
-        <b>130,000 </b>
-        <span style={{color:'red'}}></span>
-        </div>
-    )
-}
-function App(props){
-    return (
-        <div className="App" id="12345" backgroundColor="233">
-            <header className="App-header"> 
-                <SaleButton name="Quan" fName="Huynh"/>
-            </header>
-            <Image imageURL="https://hirogaru-nihongo.jp/wp/wp-content/themes/hirogaru/common/images/logo.png"/>   
-            <br/>
-            <Price></Price>
-        </div>
-    );
-}
+
 export default App;
